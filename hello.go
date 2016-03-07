7850483482cb4df45b424bf8aec9c5a12b7afa8f
KShanafelt <foxcoll@live.com>
2016-03-06 18:06:43 -0800

    first commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/kshanafelt/stringutilgo"
+)
+
+func main() {
+   // fmt.Printf("hello, world\n")
+   fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
